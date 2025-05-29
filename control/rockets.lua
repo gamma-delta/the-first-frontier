19bb8906ea79cfcd052138613d15778d9a6b3628
gamma-delta <petrathekat@gmail.com>
2025-05-29 15:44:26 -0500

    remove useless event after #10 fix

--- control/rockets.lua
+++ control/rockets.lua
@@ -30,36 +30,10 @@ local on_any_built = putil.on_any_built(function(evt)
   end
 end)
 
-local function on_rocket_launch(evt)
-  local pod = evt.rocket.attached_cargo_pod
-  if pod.name == "lunar-cargo-pod" then
-    local origin_surface_name = 
-      pod.cargo_pod_origin
-      and pod.cargo_pod_origin.surface
-      and pod.cargo_pod_origin.surface.name
-    local moony_dest = pglobals.planet_moon_map[origin_surface_name]
-    -- game.print("Found cargo pod " .. tostring(pod) .. ", sending to " .. tostring(moony_dest))
-    if moony_dest then
-      pod.cargo_pod_destination = {
-        type = defines.cargo_destination.surface,
-        surface = moony_dest
-      }
-    else
-      -- Bruh
-      pod.cargo_pod_destination = {
-        type = defines.cargo_destination.surface,
-        surface = origin_surface_name
-      }
-      game.print("How did you manage to launch a lunar rocket on [planet=" .. origin_surface_name .. "] which doesn't have a partner body??? Stop that. You're *lucky* I let you get your items back. Please open a bug report")
-    end
-  end
-end
-
 return {
   events = putil.smash_events{
     on_any_built,
     {
-      -- [defines.events.on_rocket_launch_ordered] = on_rocket_launch,
       [defines.events.on_cargo_pod_finished_ascending] = uppies,
    }
   },
