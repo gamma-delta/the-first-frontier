c4885453c27e27cc2ecabd7bc8371a7b82b041a8
gamma-delta <petrathekat@gmail.com>
2025-12-11 19:00:51 -0600

    why do my events not work

--- control.lua
+++ control.lua
@@ -1,11 +1,13 @@
 local veh = require "__core__/lualib/event_handler"
-veh.add_libraries({
+local augh = {
   require("control/freeplay"),
   require("control/dust-pollution"),
   require("control/ore-inclusions"),
   require("control/rockets"),
   require("control/demolisher-crafting"),
   require("control/misc"),
-})
+}
+log(serpent.block(augh))
+veh.add_libraries(augh)
 
 require("control/compat")
--- control/freeplay.lua
+++ control/freeplay.lua
@@ -1,19 +1,23 @@
 -- https://codeberg.org/raiguard/Krastorio2/src/branch/master/scripts/freeplay.lua
 -- see also __base__/script/freeplay/freeplay.lua for the API
 local function add_items()
-  if game.simulation then return end
-  
+  log("hello world?")
+  if game.simulation or not remote.interfaces["freeplay"] then
+    return
+  end
+
   local ship_items = remote.call("freeplay", "get_ship_items")
   -- it looks like these are distributed across all the debris
   local debris_items = remote.call("freeplay", "get_debris_items")
   -- to get you started, and because that's probably what the ship is made out of.
   -- save these for later!
   ship_items["pktff-precision-optical-component"] = 25
-  ship_items["transport-belt"] = 20
-  ship_items["iron-gear-wheel"] = 100
+  ship_items["transport-belt"] = 50
+  ship_items["assembling-machine-1"] = 5
+  ship_items["iron-plate"] = 200
+
   debris_items["pktff-aluminum-plate"] = 20
   debris_items["low-density-structure"] = 50
-  debris_items["iron-plate"] = 200
   debris_items["electronic-circuit"] = 100
   remote.call("freeplay", "set_debris_items", debris_items)
   remote.call("freeplay", "set_ship_items", ship_items)
@@ -21,5 +25,6 @@ end
 
 -- ?????
 return {
-  on_init = add_items
+  on_init = add_items,
+  on_configuration_changed = add_items,
 }
