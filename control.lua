3cf19bb9e4a07b2bd18a2d9075a2d1ff6242678b
gamma-delta <petrathekat@gmail.com>
2025-12-11 19:31:31 -0600

    make dust pollution impl less wacky

--- control.lua
+++ control.lua
@@ -1,13 +1,11 @@
 local veh = require "__core__/lualib/event_handler"
-local augh = {
+veh.add_libraries({
   require("control/freeplay"),
   require("control/dust-pollution"),
   require("control/ore-inclusions"),
   require("control/rockets"),
   require("control/demolisher-crafting"),
   require("control/misc"),
-}
-log(serpent.block(augh))
-veh.add_libraries(augh)
+  require("control/compat"),
+})
 
-require("control/compat")
